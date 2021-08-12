d3bf4e4eae5b8fc1a648fcaa20fc8bb05fb8e8d0
Khhs167 <34920681+Khhs167@users.noreply.github.com>
2021-08-12 15:01:12 +0200

    Update build.sh

--- src/build.sh
+++ src/build.sh
@@ -1,4 +1,4 @@
-rm './Build/rcc.dll'
-rctc ./Classes/main.rct -s -f -o './Build/rrc.dll'
-echo '-- [ReCT Program] --'
-dotnet './Build/rrc.dll'
+rm ./Build/rcc.dll
+rctc ./Classes/main.rct -s -f -o ./Build/rrc.dll
+echo -- [ReCT Program] --
+dotnet ./Build/rrc.dll
